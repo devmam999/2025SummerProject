9ac9c59f8c488aaccfd99efe2ceed4e3058f6ce4
devmam999 <107886293+devmam999@users.noreply.github.com>
2025-06-28 08:21:41 -0700

    Made a routing change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from './pages/SignIn.tsx';
 import SignUp from './pages/SignUp.tsx';
 import Settings from './pages/Settings.tsx';
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/settings" element={<Settings />} />
